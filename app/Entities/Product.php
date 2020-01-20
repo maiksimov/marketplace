80d0229af6787355ae41675d954442d44fd362da
Michael <somov.michail.yur@gmail.com>
2020-01-20 23:24:07 +0200

    Add relations to product

--- app/Entities/Product.php
+++ app/Entities/Product.php
@@ -19,4 +19,9 @@ class Product extends Model
     {
         return $this->belongsTo(Category::class);
     }
+
+    public function ratings()
+    {
+        return $this->hasMany(Rating::class);
+    }
 }
