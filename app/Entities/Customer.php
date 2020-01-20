03c6a6394c69a2efbb2573d21c23872ee715088e
Michael <somov.michail.yur@gmail.com>
2020-01-20 23:15:03 +0200

    Add relations to customer

--- app/Entities/Customer.php
+++ app/Entities/Customer.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends Model
 {
     protected $table = 'customers';
+    protected $fillable = ['first_name', 'last_name', 'email', 'password'];
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+
+    public function ratings()
+    {
+        return $this->hasMany(Rating::class);
+    }
 }
